59184e67ef159f21f4551666e88a69c38f704f7a
Steven Difelippo <matrixdriver04@gmail.com>
2017-09-21 14:05:48 -0400

    tests passed

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,7 @@
 import reducer from "./reducer";
 import { createTodo } from "./actions";
+import { toggleTodo } from "./actions";
+import { filterTodos } from "./actions";
 
 describe("CREATE_TODO", function () {
     test("creates a new todo", function () {
@@ -8,10 +10,22 @@ describe("CREATE_TODO", function () {
         expect(state.todos).toHaveLength(1);
         expect(state.todos[0]).toEqual({id: 1, done: false, text: "Test"});
     })
+})
 
-    test("updates nextId", function () {
+
+describe("TOGGLE_TODO", function () {
+    test("toggles a existing todo", function () {
         const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
-        expect(state.nextId).toEqual(2);
+        let state = reducer(initialState, createTodo("Test"));
+        state = reducer(state, toggleTodo(1));
+        expect(state.todos[0]).toEqual({id: 1, done: true, text: "Test"});
+    })
+})
+
+describe("FILTER_TODOS", function () {
+    test("filters an array of existing todos", function () {
+        const initialState = {filter: "all"}
+        let state = reducer(initialState, filterTodos("complete"));
+        expect(state).toEqual({filter: "complete"});
     })
 })
